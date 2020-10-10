7de2abe76093227d799983df41cd5550690744d0
guichavesreal <guigabeat64@gmail.com>
2020-10-09 21:47:17 -0300

    Initial commit

---
+++ src/test/java/com/viavarejo/backendtest/BackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viavarejo.backendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
